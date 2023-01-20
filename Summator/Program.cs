ba480a0a67fe6b05a7654d96bde320a41d9f0192
Bori&Fil <Bori&Fil@WIN-LKTNPQ21F67>
2023-01-21 00:28:42 +0200

    Add project files.

---
+++ Summator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Sumata e ravna na " + Summator.Sum(new int[] { 1, 2, 3 }));
+        }
+    }
+}
